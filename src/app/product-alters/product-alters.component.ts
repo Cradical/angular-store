1fcbfd46729bc7a1c294870b811dcfbd7087ba8e
Conrad Wright <easp.conrad@gmail.com>
2019-09-26 13:50:03 -0600

    Initial commit; basic components

---
+++ src/app/product-alters/product-alters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alters',
+  templateUrl: './product-alters.component.html',
+  styleUrls: ['./product-alters.component.css']
+})
+export class ProductAltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
