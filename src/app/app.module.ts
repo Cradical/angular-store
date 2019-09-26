db79092e8faa4d4039eeedac6bce2838d14cad57
Conrad Wright <easp.conrad@gmail.com>
2019-09-26 14:05:57 -0600

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAltersComponent } from './product-alters/product-alters.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TopBarComponent } from './top-bar/top-bar.component'
+import { ProductListComponent } from './product-list/product-list.component'
+import { ProductAltersComponent } from './product-alters/product-alters.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAltersComponent
+    ProductAltersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
