e7ca201a97b1109e7588eeb587e8892a9715e9f8
Conrad Wright <easp.conrad@gmail.com>
2019-09-27 10:39:03 -0600

    Add cart component and wire up to router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductAltersComponent } from './product-alters/product-alters.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
+import { CartComponent } from './cart/cart.component'
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     ProductAltersComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +25,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ]),
   ],
   providers: [],
