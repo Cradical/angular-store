0d30a95e77250d044101e8272ca4f42064b28721
Conrad Wright <easp.conrad@gmail.com>
2019-09-27 11:16:21 -0600

    Add and wire up shipping price component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component'
 import { ProductAltersComponent } from './product-alters/product-alters.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
 import { CartComponent } from './cart/cart.component'
+import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { CartComponent } from './cart/cart.component'
     ProductAltersComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +30,7 @@ import { CartComponent } from './cart/cart.component'
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   providers: [],
